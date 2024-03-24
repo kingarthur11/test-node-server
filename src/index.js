36d1aee0bd776aa554fd595410e7fddc32b075e1
kingarthur11 <orduharthur@gmail.com>
2024-03-24 22:35:09 +0100

    test server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// app.listen(3000, () => console.log("Server ready on port 3000."));
+
+// module.exports = app;
+
+
+const app = require('./config/express');
+const mongoose = require('./config/mongoose');
+
+// mongoose.connect();
+
+app.listen(port, () => console.info(`server started on port ${port}`));
 
 module.exports = app;
