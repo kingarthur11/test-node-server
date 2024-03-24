8b34bb8fe3f9198dd707d2d200b07a6c00289fbb
kingarthur11 <orduharthur@gmail.com>
2024-03-24 22:39:18 +0100

    test server

--- src/index.js
+++ src/index.js
@@ -13,6 +13,6 @@ const mongoose = require('./config/mongoose');
 
 // mongoose.connect();
 
-app.listen(port, () => console.info(`server started on port ${port}`));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
